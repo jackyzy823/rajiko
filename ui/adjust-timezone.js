a47c6c0b39ebb99fc967929324720b92a606693f
jackyzy823 <jackyzy823@gmail.com>
2018-04-05 13:49:22 +0800

    make script work in the right lifecyle of document

--- ui/adjust-timezone.js
+++ ui/adjust-timezone.js
@@ -1,3 +1,5 @@
-var momentscript = document.createElement("script");
-momentscript.src = chrome.extension.getURL('ui/moment-timezone.js');
-document.head.appendChild(momentscript);
+document.addEventListener("DOMContentLoaded", function(event) {
+  var momentscript = document.createElement("script");
+  momentscript.src = chrome.extension.getURL('ui/moment-timezone.js');
+  document.head.appendChild(momentscript);
+});
