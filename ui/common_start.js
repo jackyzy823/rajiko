ccad33a42ca3f2349eb8fa322742baa2b0808e97
jackyzy823 <jackyzy823@gmail.com>
2018-04-13 22:26:54 +0800

    found why recording sounds weired. moved lots of global variables into closure functions

--- ui/common_start.js
+++ ui/common_start.js
@@ -9,4 +9,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
   let inspect_script = document.createElement("script");
   inspect_script.src = chrome.extension.getURL('ui/inspect_start.js');
   document.head.appendChild(inspect_script);
+
+
+  let targetPlayButton = document.getElementById('play').getElementsByTagName('i')[0];
+  let observer = new MutationObserver(function(list){
+    for(let mutation of list){
+      if(mutation.type == 'attributes' && mutation.attributeName == 'class'){
+        if(!mutation.target.classList.contains('on')){
+           chrome.runtime.sendMessage({"stop-recording":true},function(){});
+        }
+      }
+    }
+  });
+  observer.observe(targetPlayButton,{attributes:true});
+
 });
