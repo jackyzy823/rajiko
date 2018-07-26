fd23c8332509a5d509e5868047d68a3f73f179d2
jackyzy823 <jackyzy823@gmail.com>
2018-07-26 22:49:58 +0800

    replace chrome.extension.getURL with chrome.runtime.getURL.

--- ui/common_start.js
+++ ui/common_start.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   let inspect_script = document.createElement("script");
-  inspect_script.src = chrome.extension.getURL('ui/inspect_start.js');
+  inspect_script.src = chrome.runtime.getURL('ui/inspect_start.js');
   document.head.appendChild(inspect_script);
 
 
